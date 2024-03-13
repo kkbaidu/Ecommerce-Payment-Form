d502b50ebefe6ea5eb92774816ceb6bc34e6ada8
Kingsley <kingsleybaidu99@gmail.com>
2024-03-13 11:19:42 +0000

    deployed

--- src/components/shopping-cart.tsx
+++ src/components/shopping-cart.tsx
@@ -14,12 +14,12 @@ const ShoppingCart = () => {
                         src={item.image}
                         />
                     </div>
-                    <div className="flex flex-col items-start justify-between w-[250px] h-[110px] pb-7 pt-2">
+                    <div className="flex flex-col items-start justify-between w-[230px] md:w-[400px] lg:w-[400px] h-[110px] pb-7 pt-2">
                         <span className="font-bold"> {item.aboutItem.name} </span>
                         <span className="text-[13px]"> {item.aboutItem.description} </span>
                         <span className="text-[13px]"> ID: <span> {item.aboutItem.id} </span> </span>
                     </div>
-                    <div className="flex flex-col items-start h-[110px] justify-between pb-7 pt-2">
+                    <div className="flex flex-col items-start h-[110px] justify-between pb-7 pt-2 w-[80px] md:w-auto lg:w-auto">
                         <span className="font-bold"> ${item.purchaseDetails.price} </span>
                         <span className="text-[13px]"> Size: <span className="text-[#396aff]"> {item.purchaseDetails.size} IT </span> </span>
                         <span className="text-[13px]"> QTY: <span className="text-[#396aff]"> {(item.purchaseDetails.quantity.toString()).length === 1 ? `0${item.purchaseDetails.quantity}` : item.purchaseDetails.quantity } </span> </span>
